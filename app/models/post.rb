d3eb1774958a6a76c87bae9202c39068d8f0af1e
Matt <mattfsewell@gmail.com>
2021-02-10 06:08:06 -0500

    commit tree e5b8ef83328ecf1e730d510f52ddd3b2bf3d0a30

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-
+  
+  def categories_attributes=(category_hash)
+    category_hash.each do |i, cat|
+      category = Category.find_or_create_by(name: cat[:name])
+      self.post_categories.build(:category => category)
+    end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.destroy_all
+Post.destroy_all
+User.destroy_all
+
+post1 = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+user1 = User.create(username: 'CoolGuy5')
+comment3 = Comment.create(content: "And another thing, how come there aren't any vegetabls at the movie theater...", user: user1, post: post1)
+category1 = Category.create(name: 'Cool')
+post1.categories << category1
+@category = Category.create(name: "Lame")
+      @post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+      @post.categories << @category
+      @user = User.create(username: 'RealBigFish')
+      @comment1 = Comment.create(content: "This was great!", user: @user, post: @post)
+      @comment2 = Comment.create(content: "And another thing, how come there aren't any vegetabls at the movie theater...", user: @user, post: @post)
